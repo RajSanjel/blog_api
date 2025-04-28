168501916e0a11e780e231fc8074e97394f20a6c
Raj Sanjel <rajsanjel201@gmail.com>
2025-04-28 22:32:09 +0545

    feat: CORS

--- src/routes/router.rs
+++ src/routes/router.rs
@@ -5,11 +5,17 @@ use crate::handlers::post::{blog_post, get_blog};
 use crate::handlers::user::get_user;
 use crate::middleware::verify_user::verify_user;
 
-use axum::middleware;
 use axum::{Router, routing::get, routing::post};
+use axum::{http, middleware};
 use tower_cookies::CookieManagerLayer;
+use tower_http::cors::CorsLayer;
 
 pub fn create_router(pool: DbPool) -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin(["http://localhost:5500".parse().unwrap()]) // assuming it's our frontend
+        .allow_methods([http::Method::GET, http::Method::POST, http::Method::OPTIONS])
+        .allow_credentials(true);
+
     let auth_router = Router::new()
         .route("/register", post(register))
         .route("/login", post(login))
@@ -30,4 +36,5 @@ pub fn create_router(pool: DbPool) -> Router {
         .nest("/api", protected_api_router)
         .with_state(pool)
         .layer(CookieManagerLayer::new())
+        .layer(cors)
 }
